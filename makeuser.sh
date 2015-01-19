18648ad55259b76c24cace0192c12da620b12593
jfeldis <jfeldis@gmail.com>
2015-01-18 16:20:30 -0800

    Create makeuser.sh

---
+++ makeuser.sh
@@ -0,0 +1,5 @@
+sudo adduser crypti
+sudo cp -r /home/linaro/.[a-zA-Z0-9]* /home/crypti
+sudo cp -r /home/linaro/* /home/crypti
+sudo chown -R crypti:users /home/crypti/.*
+sudo chown -R crypti:users /home/crypti/*
