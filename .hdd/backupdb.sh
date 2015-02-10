fe85703000c7616e882998cad05d818c621261b5
jfeldis <jfeldis@gmail.com>
2015-02-10 00:43:51 -0800

    Update backupdb.sh

--- .hdd/backupdb.sh
+++ .hdd/backupdb.sh
@@ -5,13 +5,13 @@ if [ "$x" == "" ]; then
   echo "btc not running $(date)" >> bak.log 
 else
   echo "stop bitcoind to run backup"  
-  sh /home/linaro/hdd/btcstop.sh
+  sh /home/linaro/btcstop.sh
   sleep 5s 
   echo "backing up db"
-  rsync --checksum -r /home/linaro/hdd/.bitcoin /home/linaro/hdd/livebak
-  echo $(date) >> /home/linaro/hdd/cron.log
-  echo "db backup run $(date)" >> /home/linaro/hdd/cron.log
-  echo "db backup run $(date)" >> /home/linaro/hdd/bak.log
+  rsync --checksum -r /home/linaro/.bitcoin /home/linaro/livebak
+  echo $(date) >> /home/linaro/cron.log
+  echo "db backup run $(date)" >> /home/linaro/cron.log
+  echo "db backup run $(date)" >> /home/linaro/bak.log
   echo "backup complete. restart bitcoind"
-  sh /home/linaro/hdd/btcstart.sh
+  sh /home/linaro/btcstart.sh
 fi
