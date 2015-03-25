bc04388b91b9fb5fb0079abf94c5fc231eaca429
jfeldis <jfeldis@gmail.com>
2015-03-25 00:13:06 -0700

    Update getdb.sh

--- setup/getdb.sh
+++ setup/getdb.sh
@@ -1,3 +1,3 @@
- sudo rsync --checksum -rv linaro@192.168.1.146:/home/linaro/livebak/ /home/linaro
- sudo chown -R linaro:linaro /home/linaro
+sudo rsync --checksum -rv linaro@192.168.1.146:/home/linaro/livebak/ /home/linaro
+sudo chown -R linaro:linaro /home/linaro
 echo "blockchain database copy complete" >> /home/linaro/setup.log
