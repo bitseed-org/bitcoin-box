b5cb3a060fdb96442b98e44438d4fdb4478ae7f2
jfeldis <jfeldis@gmail.com>
2015-03-25 00:14:31 -0700

    Update getdb.sh

--- setup/getdb.sh
+++ setup/getdb.sh
@@ -1,3 +1,4 @@
-sudo rsync --checksum -rv linaro@192.168.1.146:/home/linaro/livebak/ /home/linaro
+echo "copy blockchain to .bitcoin directory.  wait 1-2 hours"
+sudo rsync -rv linaro@192.168.1.146:/home/linaro/livebak/ /home/linaro
 sudo chown -R linaro:linaro /home/linaro
 echo "blockchain database copy complete" >> /home/linaro/setup.log
