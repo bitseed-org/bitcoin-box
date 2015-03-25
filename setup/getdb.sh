363b52229fd4ae929aeb97946825c178066a8fc5
jfeldis <jfeldis@gmail.com>
2015-03-24 23:44:49 -0700

    Update getdb.sh

--- setup/getdb.sh
+++ setup/getdb.sh
@@ -1,1 +1,2 @@
-scp -r linaro@192.168.1.224:/home/linaro/livebak /home/linaro/
+ sudo rsync --checksum -rv linaro@192.168.1.146:/home/linaro/livebak/ /home/linaro
+ sudo chown -R linaro:linaro /home/linaro
