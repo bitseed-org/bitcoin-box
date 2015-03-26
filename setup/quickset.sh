3e41046105d0285d832d018ea4df064e2d20fa37
jfeldis <jfeldis@gmail.com>
2015-03-26 13:11:10 -0700

    Update quickset.sh

--- setup/quickset.sh
+++ setup/quickset.sh
@@ -1,1 +1,10 @@
 sudo rm /etc/mac
+cp /home/linaro/bitcoin-box/.hdd/*.sh /home/linaro
+sudo chown -R linaro:linaro /home/linaro
+chmod 755 /home/linaro/*.sh
+
+#disable screensaver becuase it uses too much CPU
+rm /home/linaro/.config/lxsession/LXDE/autostart
+echo "@lxpanel --profile LXDE" >> /home/linaro/.config/lxsession/LXDE/autostart
+echo "@pcmanfm --desktop --profile LXDE" >> /home/linaro/.config/lxsession/LXDE/autostart
+echo "@/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1" >> /home/linaro/.config/lxsession/LXDE/autostart
