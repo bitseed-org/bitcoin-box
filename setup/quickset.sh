ba6f55e5b61855e190af571179d97f74c4915b90
jfeldis <jfeldis@gmail.com>
2015-03-26 20:41:18 -0700

    Update quickset.sh

--- setup/quickset.sh
+++ setup/quickset.sh
@@ -8,3 +8,11 @@ rm /home/linaro/.config/lxsession/LXDE/autostart
 echo "@lxpanel --profile LXDE" >> /home/linaro/.config/lxsession/LXDE/autostart
 echo "@pcmanfm --desktop --profile LXDE" >> /home/linaro/.config/lxsession/LXDE/autostart
 echo "@/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1" >> /home/linaro/.config/lxsession/LXDE/autostart
+
+#dd line below not needed if hdd is pre-imaged with swapfile
+echo "seting up swap"
+dd if=/dev/zero of=/home/swapfile bs=1024 count=1048576
+sudo chown root:root /home/swapfile
+sudo chmod 0600 /home/swapfile
+sudo mkswap /home/swapfile
+sudo swapon  /home/swapfile
