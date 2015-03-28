de6f7a43e6e55562265aaceadfb3c4b4c90fbca8
jfeldis <jfeldis@gmail.com>
2015-03-28 00:30:52 -0700

    Update btcsetup.sh

--- setup/btcsetup.sh
+++ setup/btcsetup.sh
@@ -45,6 +45,7 @@ cd ~
 cp -r /home/linaro/bitcoin-box/.hdd/* /home/linaro
 chmod 755 /home/linaro/*.sh
 
+#disable HDD disk write cache - reduce chance of corruption due to power loss
 sudo chown linaro:linaro /etc/hdparm.conf
 echo 'write_cache = off' >> write_cache = off
 sudo chown root:root /etc/hdparm.conf
