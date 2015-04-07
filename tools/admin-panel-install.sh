714cb1efe4448c1d585c224d1c10c5e5498d7794
Jay Feldis <jfeldis@bitseed.org>
2015-04-07 02:02:19 -0700

    Update admin-panel-install.sh

--- tools/admin-panel-install.sh
+++ tools/admin-panel-install.sh
@@ -1,5 +1,6 @@
 #run as sudo
 #installs admin panel into /var/www/html
+sudo apt-get install -y php5 php5-curl
 git clone https://github.com/BitSeed-org/BitcoinNodeAdmin
-sudo cp -r BitcoinNodeAdmin/wallet_dark /var/www/html/
-sudo chown www-date:www-data /var/www/html
+sudo cp -r BitcoinNodeAdmin/wallet_dark/* /var/www/html
+sudo chown www-data:www-data /var/www/html
