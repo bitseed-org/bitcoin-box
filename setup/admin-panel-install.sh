471dcb823fe1d5c6b0de1ea68bd680cd49c1229d
Jay Feldis <jfeldis@bitseed.org>
2015-04-08 01:28:24 -0700

    Update admin-panel-install.sh

--- setup/admin-panel-install.sh
+++ setup/admin-panel-install.sh
@@ -6,5 +6,17 @@ sudo cp -r BitcoinNodeAdmin/wallet_dark/* /var/www/html
 sudo chown www-data:www-data /var/www/html
 echo "replace this line in /home/linaro/reward-addr with bitcoin address" > /home/linaro/reward-addr
 
+-------------------------------------------
 #Upgrading Bitseed devcies S/N 30 and earlier
-
+-----------------------------------------
+#stop bitcoin via ./btcstop.sh
+#edit ~/.bitcoin/bitcoin.conf and change the rpcpassword to "bitseed"
+#(the rpcpassword in bitcoin.conf and conifig.inc.php must the the same)
+#upgrade scripts
+#rm -rf bitcoin-box
+#./gitclone.sh
+#cd bitcoin-box/setup
+#cp /home/linaro/bitcoin-box/.hdd/*.sh /home/linaro
+#sudo chown -R linaro:linaro /home/linaro
+#chmod 755 /home/linaro/*.sh
+#sudo ./admin-panel-install.sh
