d3018adcad1c578d5a107ad0c7845a90d7f78e31
Jay Feldis <jfeldis@bitseed.org>
2015-04-08 00:20:49 -0700

    Update admin-panel-install.sh

--- setup/admin-panel-install.sh
+++ setup/admin-panel-install.sh
@@ -4,3 +4,7 @@ sudo apt-get install -y php5 php5-curl
 git clone https://github.com/BitSeed-org/BitcoinNodeAdmin
 sudo cp -r BitcoinNodeAdmin/wallet_dark/* /var/www/html
 sudo chown www-data:www-data /var/www/html
+echo "replace this line in /home/linaro/reward-addr with bitcoin address" > /home/linaro/reward-addr
+
+#Upgrading Bitseed devcies S/N 30 and earlier
+
