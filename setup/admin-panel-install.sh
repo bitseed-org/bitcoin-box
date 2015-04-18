b39bf87c6a3ace7446523454ccb10a9cb905a07c
Jameson Lopp <jameson.lopp@gmail.com>
2015-04-18 10:26:18 -0400

    adding missing steps to admin panel install

--- setup/admin-panel-install.sh
+++ setup/admin-panel-install.sh
@@ -4,7 +4,9 @@
 git clone https://github.com/BitSeed-org/BitcoinNodeAdmin
 sudo cp -r BitcoinNodeAdmin/wallet_dark/* /var/www/html
 sudo chown www-data:www-data /var/www/html
-echo "replace this line in /home/linaro/reward-addr with bitcoin address" > /home/linaro/reward-addr
+sudo /etc/init.d/apache2 restart
+echo "edit /home/linaro/reward-addr with your bitcoin address for the bitnodes incentive program"
+echo "replace this line with your bitcoin address" > /home/linaro/reward-addr
 
 echo "web admin install done" > /home/linaro/bitcoin-box/setup/setup.log
 
@@ -13,12 +15,13 @@ echo "web admin install done" > /home/linaro/bitcoin-box/setup/setup.log
 #-----------------------------------------
 #stop bitcoin via ./btcstop.sh
 #edit ~/.bitcoin/bitcoin.conf and change the rpcpassword to "bitseed"
-#(the rpcpassword in bitcoin.conf and conifig.inc.php must the the same)
+#(the rpcpassword in bitcoin.conf and config.inc.php must the the same)
 #upgrade scripts
 #rm -rf bitcoin-box
 #./gitclone.sh
-#cd bitcoin-box/setup
 #cp /home/linaro/bitcoin-box/.hdd/*.sh /home/linaro
 #sudo chown -R linaro:linaro /home/linaro
 #chmod 755 /home/linaro/*.sh
+#cd bitcoin-box/setup
+#chmod 755 ./admin-panel-install.sh
 #sudo ./admin-panel-install.sh
