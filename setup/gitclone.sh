0a4c9dd5a99bc0c62a58e5cde80cd7d49aef8fee
Jay Feldis <jfeldis@bitseed.org>
2015-06-14 13:00:43 -0700

    Create gitclone.sh

---
+++ setup/gitclone.sh
@@ -0,0 +1,7 @@
+#gets latest code from github
+git clone https://github.com/BitSeed-org/bitcoin-box
+sudo chmod 755 /home/linaro/bitcoin-box/setup/quickset.sh
+cd bitcoin-box/setup
+
+echo "gitclone done" > /home/linaro/bitcoin-box/setup/setup.log
+
