047306d997b9eb34d29f3ef220899e1508adff94
Jay Feldis <jfeldis@bitseed.org>
2015-07-04 14:50:24 -0700

    Update blockstatus.sh

--- .hdd/blockstatus.sh
+++ .hdd/blockstatus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #get current block height from blockchain.info
 echo $(date)
-wget https://blockchain.info/q/getblockcount &> /dev/null
+wget -4 https://blockchain.info/q/getblockcount &> /dev/null
 netblk=$(<getblockcount)
 echo "Current Block: $netblk"
 
