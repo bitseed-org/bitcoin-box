0e7653fbcd9fd3637f0c1ba7acf1ec271a3503c6
Jay Feldis <jfeldis@bitseed.org>
2015-07-21 13:56:11 -0700

    Update bitcoin-upgrade.sh

--- tools/bitcoin-upgrade.sh
+++ tools/bitcoin-upgrade.sh
@@ -1,5 +1,6 @@
 #downloads latest bitcoin arm binaries and installes on bitseed
 echo "upgrading bitcoin core"
+sh /home/linaro/btcstop.sh
 wget -4 https://bitseed.org/device/latestversion/bitcoinarm.zip
 rm bitcoind
 rm bitcoin-cli
@@ -8,3 +9,4 @@ rm bitcoin-qt
 unzip bitcoinarm.zip
 rm bitcoinarm.zip
 echo "bitcoin core upgraded"
+echo "restarting bitcoind.  Allow 15 minutes for bitcoin to startup"
