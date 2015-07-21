f99c04cd7088fa95c172e9cc165306bc636757de
Jay Feldis <jfeldis@bitseed.org>
2015-07-21 13:50:37 -0700

    Create bitcoin-upgrade.sh

---
+++ tools/bitcoin-upgrade.sh
@@ -0,0 +1,9 @@
+#downloads latest bitcoin arm binaries and installes on bitseed
+echo "upgrading bitcoin core"
+wget -4 https://bitseed.org/device/latestversion/bitcoinarm.zip
+rm bitcoind
+rm bitcoin-cli
+rm bitcoin-tx
+rm bitcoin-qt
+unzip bitcoinarm.zip
+echo "bitcoin core upgraded"
