6da2b560a327beaa433282f16de3a275b819db20
Jay Feldis <jfeldis@bitseed.org>
2015-07-25 11:39:32 -0700

    Update bitnodesetup.sh

--- tools/bitnodesetup.sh
+++ tools/bitnodesetup.sh
@@ -1,7 +1,9 @@
+#this script automates bitnodes registration
+echo "Bitnodes requires that you publish a web page at the same public IP address as your node. A page will be served fron the bitseed device on port 80, but you may need to manually set you router to map the bitseed page to an external port"
 wget http://ipecho.net/plain &> /dev/null
 ipadr=$(<plain)
 echo "External IP address is:$ipadr"
-echo -n "Enter port of web page showing BTC address (80 default):"
+echo -n "Enter the external port of web page showing BTC address (80 default):"
 read prt
 ipadrpt=$ipadr-$prt
 echo "$ipadrpt"
@@ -18,6 +20,6 @@ echo "$btcaddress"
 curl --ipv4 -H 'Accept: application/json; indent=4' -d '$btcaddress' -d '$ur' https://getaddr.bitnodes.io/$
 rm plain
 #curl --ipv4 -H 'Accept: application/json; indent=4' -d 'bitcoin_address=1DXam6ZsoajEihNuVqdGUWQLUDC1Yph4f$
-
+echo "bitnodes registration sent.  If successful, the bitnodes status in the GUI will show as "verified" within about 5-10 minutyes"
 
 
