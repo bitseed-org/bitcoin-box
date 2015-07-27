640e34da21edf638524902111a72a1962e9e8c8f
Jay Feldis <jfeldis@bitseed.org>
2015-07-26 17:29:01 -0700

    Create restartbtc.sh

---
+++ .hdd/restartbtc.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+#stops and starts bitcoind when restartflag file is set to 1
+#does nothing in all other cases
+#intended to be run every minute by cronttab
+rsflag=$( < /home/linaro/restartflag)
+echo "Flag= $rsflag"
+if (( rsflag == 1 )); then
+ echo "restarting bitcoind, please wait"
+echo 0 > /home/linaro/restartflag
+/home/linaro/bitcoin-cli stop
+echo "Do not shut down the device until notified"
+$x=$(pgrep -f bitcoind)
+while [ "$x" !=  "" ]
+do
+  echo -n "."
+  sleep 1s
+  x=$(pgrep -f bitcoind)
+done
+echo "bitcoin has stopped. restart in 5 seconds"
+sleep 10s
+echo "starting bitcoind, pleese wait 15 min"
+/home/linaro/bitcoind -daemon
+fi
