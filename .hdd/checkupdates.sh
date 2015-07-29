74c326313e10f30b44f74d572ace870e16ad6d6a
Jay Feldis <jfeldis@bitseed.org>
2015-07-28 19:17:53 -0700

    Update checkupdates.sh

--- .hdd/checkupdates.sh
+++ .hdd/checkupdates.sh
@@ -2,7 +2,7 @@
 #downloads an update notification script from bitseed.org
 #this script is run by crontab
 rm /home/linaro/v1update.sh
-wget https://www.bitseed.org/device/updater/v1update.sh
-chmod 755 /home/linaro/v1update.sh
+wget https://www.bitseed.org/device/updater/v2update.sh
+chmod 755 /home/linaro/v2update.sh
 echo "update check $(date)" >> cron.log
 exit
