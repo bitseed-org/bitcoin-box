9775b4c8fd8f54c857cfaa456488683501fa95a6
Jay Feldis <jfeldis@bitseed.org>
2015-10-06 16:27:48 -0700

    Update quickset.sh

--- setup/quickset.sh
+++ setup/quickset.sh
@@ -40,4 +40,5 @@ sudo swapon  /home/swapfile
 
 echo "200" > /home/linaro/version
 sudo chown linaro:linaro /home/linaro/checkupdates.sh
+sudo chmod u+s /bin/ping
 echo "quickset done" >> /home/linaro/bitcoin-box/setup/setup.log
