6d8c1093bf6db17098c21343176d280b66289f35
Jay Feldis <jfeldis@bitseed.org>
2015-10-08 17:11:13 -0700

    Update xt11b-install.sh

--- setup/xt11b-install.sh
+++ setup/xt11b-install.sh
@@ -5,6 +5,7 @@ sleep 10s
 #get XT binary files from bitseed.org
 rm bitcoin-xt-0.11a.zip
 rm -rf bitcoin-xt-0.11a
+rm XT11B-binaries.zip
 wget https://www.bitseed.org/device/bitcoin-xt-0.11b/XT11B-binaries.zip
 
 #stop bitcoin
@@ -19,6 +20,8 @@ rm /home/linaro/bitcoin-cli
 unzip XT11B-binaries.zip
 cp XT11B-binaries/bitcoind  /home/linaro
 cp XT11B-binaries/bitcoin-cli  /home/linaro
+
+rm XT11B-binaries.zip
 
 #restart bitcoind
 echo "XT installed, bitcoin will now restart.  Allow about 10 minutes before it is active"
