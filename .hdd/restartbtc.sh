73677bd22ddadc339ad4b4408ef78b33e45c4c2c
Jay Feldis <jfeldis@bitseed.org>
2015-10-09 01:02:25 -0700

    Update restartbtc.sh

--- .hdd/restartbtc.sh
+++ .hdd/restartbtc.sh
@@ -7,29 +7,30 @@
 rsflag=$( < /home/linaro/restartflag)
 #echo "Flag= $rsflag"
 if (( rsflag == 4 )); then
- echo "power-up sequence, please wait"
- echo 0 > /home/linaro/restartflag
- sleep 2m
- ./btcwatch.sh
-else
-if (( rsflag == 1 )); then
- echo "restarting bitcoind, please wait"
- echo 0 > /home/linaro/restartflag
- /home/linaro/bitcoin-cli stop
- echo "Do not shut down the device until notified"
- t=0
- m=300
- x=$(pgrep -f bitcoind)
- while [ "$x" !=  "" -a "$t" -lt "$m" ]
- do
-  echo -n "."
-  sleep 1s
-  t=t+1
+  echo "power-up sequence, please wait"
+  echo 0 > /home/linaro/restartflag
+  sleep 2m
+  ./btcwatch.sh
+ else
+ if (( rsflag == 1 )); then
+  echo "restarting bitcoind, please wait"
+  echo 0 > /home/linaro/restartflag
+  /home/linaro/bitcoin-cli stop
+  echo "Do not shut down the device until notified"
+  t=0
+  m=300
   x=$(pgrep -f bitcoind)
- done
- kill -9 "$x"
- echo "bitcoin has stopped. restart in 5 seconds"
- sleep 10s
- echo "starting bitcoind, pleese wait 15 min"
- /home/linaro/bitcoind -daemon
+  while [ "$x" !=  "" -a "$t" -lt "$m" ]
+  do
+   echo -n "."
+   sleep 1s
+   t=t+1
+   x=$(pgrep -f bitcoind)
+  done
+  kill -9 "$x"
+  echo "bitcoin has stopped. restart in 5 seconds"
+  sleep 10s
+  echo "starting bitcoind, pleese wait 15 min"
+  /home/linaro/bitcoind -daemon
+ fi
 fi
