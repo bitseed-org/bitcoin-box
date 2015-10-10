1e4680ce0f031ccd90d52e0c3cdca99c2cafb05e
Jay Feldis <jfeldis@bitseed.org>
2015-10-10 16:45:28 -0700

    Update memwatch.sh

--- .hdd/memwatch.sh
+++ .hdd/memwatch.sh
@@ -4,7 +4,7 @@
 byt=$(cat mempool | jq '.bytes')
 echo "$byt"
 if [ "$byt" > 200000000 ]; then
- echo "$date mempool is $byt, restarting" >> cron.log
+ echo "$(date) mempool is $byt, restarting" >> cron.log
  echo 1 > $HOME/restartflag
  echo 0 > mempool
  else
