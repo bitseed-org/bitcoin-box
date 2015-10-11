b81e86ad5ba4191208029f469d5aec78c5d61361
Jay Feldis <jfeldis@bitseed.org>
2015-10-11 11:33:51 -0700

    increase version to 201

--- setup/quickset.sh
+++ setup/quickset.sh
@@ -38,7 +38,7 @@ sudo chmod 0600 /home/swapfile
 sudo mkswap /home/swapfile
 sudo swapon  /home/swapfile
 
-echo "200" > /home/linaro/version
+echo "201" > /home/linaro/version
 sudo chown linaro:linaro /home/linaro/checkupdates.sh
 sudo cp $HOME/bitcoind /usr/local/bin
 sudo cp $HOME/bitcoin-cli /usr/local/bin
