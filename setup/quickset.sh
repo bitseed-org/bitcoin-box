2c4922eafbfdcfb2a875469b411acc025fc562ee
Jay Feldis <jfeldis@bitseed.org>
2015-10-12 16:12:45 -0700

    Update quickset.sh

--- setup/quickset.sh
+++ setup/quickset.sh
@@ -45,4 +45,5 @@ sudo cp $HOME/bitcoin-cli /usr/local/bin
 
 sudo chmod u+s /bin/ping
 sudo apt-get install jq
+sudo apt-get -y autoremove
 echo "quickset done" >> /home/linaro/bitcoin-box/setup/setup.log
