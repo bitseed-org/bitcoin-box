14eaa615491d11d2ba51d5768d19bb0a1dbe6bd8
Jay Feldis <jfeldis@bitseed.org>
2015-10-27 13:33:56 -0700

    change to watch free memory

--- .hdd/memwatch.sh
+++ .hdd/memwatch.sh
@@ -1,10 +1,25 @@
+#!/bin/bash
+
 #watch the mempool size
 #restart if it gets too big
-./bitcoin-cli getmempoolinfo > mempool
-byt=$(cat mempool | jq '.bytes')
-echo "$byt"
-if [ "$byt" -gt "180000000" ]; then
- echo "$(date) mempool is $byt, restarting" >> cron.log
+#./bitcoin-cli getmempoolinfo > mempool
+#byt=$(cat mempool | jq '.bytes')
+#echo "$byt"
+#echo "$byt" >> cron.log
+#if [ "$byt" -gt "100000000" ]; then
+# echo "$(date) mempool is $byt, restarting" >> cron.log
+# echo 1 > $HOME/restartflag
+# echo 0 > mempool
+#fi
+
+#watch device free memory.  
+#restart bitcoind if less than 50MB free
+fm=$(free | grep Mem | awk '{print $4/$2 * 1000.0}' | bc)
+a=$(echo "$fm < 50" | bc)
+#fm=$(cat freemem)
+echo "$fm" >> cron.log
+if [ "$a" -eq "1" ]; then
+ echo "$(date) freemem is $fm, restarting" >> cron.log
  echo 1 > $HOME/restartflag
- echo 0 > mempool
 fi
+
