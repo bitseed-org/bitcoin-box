322c15c41d64399c73375ca2829ad4cae622b4d0
Jay Feldis <jfeldis@bitseed.org>
2015-10-29 18:32:38 -0700

    Update torsetup.sh

--- tools/torsetup.sh
+++ tools/torsetup.sh
@@ -10,3 +10,15 @@ gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y tor deb.torproject.org-keyring
+
+#setup hidden service
+sudo echo "HiddenServiceDir /var/lib/tor/bitcoin-service/" >> /etc/tor/torrc
+sudo echo "HiddenServicePort 8333 127.0.0.1:8333" >> /etc/tor/torrc
+sudo service tor reload
+sleep 15s
+#get onion address
+sudo cat /var/lib/tor/bitcoin-service/hostname > oname
+onionname=$(cat oname)
+rm oname
+#out onion address into bitcoin.conf
+echo "externalip=$onionname" >> $HOME/.bitcoin/bitcoin.conf
