dbd7b047c11a2b0ac84eb7ee8677a226aca6e96f
Jay Feldis <jfeldis@bitseed.org>
2015-10-29 18:21:30 -0700

    Create torsetup.sh

---
+++ tools/torsetup.sh
@@ -0,0 +1,10 @@
+#
+#installs Tor and configures bitcoind to run as a Tor hidden service
+#run as sudo
+
+#Install Tor
+sudo rm /etc/apt/sources.list.d/tor.list
+sudo echo "deb http://deb.torproject.org/torproject.org trusty main"  >> /etc/apt/sources.list.d/tor.list
+sudo echo "deb-src http://deb.torproject.org/torproject.org trusty main"  >> /etc/apt/sources.list.d/tor.list
+gpg --keyserver keys.gnupg.net --recv 886DDD89
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
