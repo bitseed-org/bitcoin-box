280e9ba64a3501eadee57093bb2dd0f6274ceb86
Jay Feldis <jfeldis@bitseed.org>
2015-11-04 11:59:54 -0800

    Update memwatch.sh
    change to check free memory plus buffers <100MB

--- .hdd/memwatch.sh
+++ .hdd/memwatch.sh
@@ -14,12 +14,13 @@
 
 #watch device free memory.  
 #restart bitcoind if less than 50MB free
-fm=$(free | grep Mem | awk '{print $4/$2 * 10000.0}' | bc)
-a=$(echo "$fm < 50" | bc)
+#fm=$(free | grep Mem | awk '{print $4/$2 * 1000.0}' | bc)
+fm=$(awk '/^-/ {print $4}' <(free -m))
+a=$(echo "$fm < 100" | bc)
 #fm=$(cat freemem)
 echo "$fm" >> cron.log
 if [ "$a" -eq "1" ]; then
- echo "$(date) freemem is $fm, restarting" >> cron.log
+ echo "$(date) free mem + buffers is $fm, restarting" >> cron.log
  echo 1 > $HOME/restartflag
 fi
 
