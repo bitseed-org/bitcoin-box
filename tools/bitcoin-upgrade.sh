6b8520021ce71a243a2e8ec6b56682b70c105859
Jay Feldis <jfeldis@bitseed.org>
2015-11-16 00:19:39 -0800

    Update bitcoin-upgrade.sh

--- tools/bitcoin-upgrade.sh
+++ tools/bitcoin-upgrade.sh
@@ -8,6 +8,8 @@ rm bitcoin-tx
 rm bitcoin-qt
 unzip bitcoinarm.zip
 rm bitcoinarm.zip
+chmod 755 bitcoind
+chmod 755 bitcoin-cli
 echo "bitcoin core upgraded"
 echo "restarting bitcoind.  Allow 15 minutes for bitcoin to startup"
 sh /home/linaro/btcstart.sh
