fa3413391bfb8ee84b04ddb42e69b6913650fe82
Jay Feldis <jfeldis@bitseed.org>
2015-12-21 23:59:34 -0800

    Update torsetup.sh

--- tools/torsetup.sh
+++ tools/torsetup.sh
@@ -3,7 +3,7 @@
 
 #add Tor repository sources
 sudo echo "deb http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list
-sudo echo "deb-src http://deb.torproject.org/torproject.org trusty main"  >> /etc/apt/sources.list
+sudo echo "deb-src http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list
 gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 sudo apt-get update
