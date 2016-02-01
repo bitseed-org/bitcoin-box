658188ed9115f2cc4c36b06036ccad9243dd480c
Jay Feldis <jfeldis@bitseed.org>
2016-02-01 14:22:39 -0800

    Update memwatch.sh

--- .hdd/memwatch.sh
+++ .hdd/memwatch.sh
@@ -6,7 +6,7 @@
 fm=$(awk '/^-/ {print $4}' <(free -m))
 a=$(echo "$fm < 100" | bc)
 #fm=$(cat freemem)
-echo "$fm" >> cron.log
+#echo "$fm" >> cron.log
 if [ "$a" -eq "1" ]; then
  echo "$(date) free mem + buffers is $fm, restarting" >> cron.log
  echo 1 > $HOME/restartflag
