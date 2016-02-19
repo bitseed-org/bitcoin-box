8d82b6bf2556dec1f412ff2945e3e53857701bef
Jay Feldis <jfeldis@bitseed.org>
2016-02-19 13:53:04 -0800

    Update quickset.sh

--- setup/quickset.sh
+++ setup/quickset.sh
@@ -54,6 +54,7 @@ sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com ADCE2AF3A4E0014F
 sudo sed -i '/wiimu/d' /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y libc-dev-bin libc6 libc6-armel libc6-dev
+sleep 5
 sudo dpkg -l libc6 libc6-dev libc6-armel libc-dev-bin >> /home/linaro/bitcoin-box/setup/setup.log
 sudo dpkg -l libc6 libc6-dev libc6-armel libc-dev-bin
 
