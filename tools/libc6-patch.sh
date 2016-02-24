9788f2a8ac45b48620e6fa606263a815214c10ed
Jay Feldis <jfeldis@bitseed.org>
2016-02-23 17:06:54 -0800

    Create libc6-patch.sh

---
+++ tools/libc6-patch.sh
@@ -0,0 +1,13 @@
+#security patch to upfate glibc library
+#security patch
+sudo echo "deb http://ports.ubuntu.com/ubuntu-ports/ trusty-security main universe" >> /etc/apt/sources.list
+sudo echo "deb-src http://ports.ubuntu.com/ubuntu-ports/ trusty-security main universe" >> /etc/apt/sources.list
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com ADCE2AF3A4E0014F
+sudo sed -i '/wiimu/d' /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install -y libc-dev-bin libc6 libc6-armel libc6-dev
+sleep 5
+sudo dpkg -l libc6 libc6-dev libc6-armel libc-dev-bin >> /home/linaro/bitcoin-box/setup/setup.log
+sudo dpkg -l libc6 libc6-dev libc6-armel libc-dev-bin
+sudo apt-get install -y libevent-dev
+echo "glibc (libc6) patch completed.  reboot is recommended"
