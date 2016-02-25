db87a02a1ae4a1635d8e91011d65fb2d4a62b1cd
Jay Feldis <jfeldis@bitseed.org>
2016-02-24 20:18:57 -0800

    Update quickset.sh

--- setup/quickset.sh
+++ setup/quickset.sh
@@ -55,15 +55,15 @@ sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 74A941BA219EC810
 sudo apt-get update
 
 #security patch
-#sudo echo "deb http://ports.ubuntu.com/ubuntu-ports/ trusty-security main universe" >> /etc/apt/sources.list
-#sudo echo "deb-src http://ports.ubuntu.com/ubuntu-ports/ trusty-security main universe" >> /etc/apt/sources.list
-#sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com ADCE2AF3A4E0014F
-#sudo sed -i '/wiimu/d' /etc/apt/sources.list
-#sudo apt-get update
-#sudo apt-get install -y libc-dev-bin libc6 libc6-armel libc6-dev
-#sleep 5
-#sudo dpkg -l libc6 libc6-dev libc6-armel libc-dev-bin >> /home/linaro/bitcoin-box/setup/setup.log
-#sudo dpkg -l libc6 libc6-dev libc6-armel libc-dev-bin
+sudo echo "deb http://ports.ubuntu.com/ubuntu-ports/ trusty-security main universe" >> /etc/apt/sources.list
+sudo echo "deb-src http://ports.ubuntu.com/ubuntu-ports/ trusty-security main universe" >> /etc/apt/sources.list
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com ADCE2AF3A4E0014F
+sudo sed -i '/wiimu/d' /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install -y libc-dev-bin libc6 libc6-armel libc6-dev
+sleep 5
+sudo dpkg -l libc6 libc6-dev libc6-armel libc-dev-bin >> /home/linaro/bitcoin-box/setup/setup.log
+sudo dpkg -l libc6 libc6-dev libc6-armel libc-dev-bin
 
 
 sudo chmod u+s /bin/ping
