72b0eff1ee11ec4e2cba6c8ac879e6ea71fa5b47
Jay Feldis <jfeldis@bitseed.org>
2016-03-08 20:38:15 -0800

    convert from bitcoind to $coind

--- .hdd/restartbtc.sh
+++ .hdd/restartbtc.sh
@@ -4,22 +4,31 @@
 #does nothing in all other cases
 #intended to be run every minute by cronttab:
 #* * * * * bash /home/linaro/restartbtc.sh
+
+#which coin are we running?
+coin=$( < $HOME/coin)
+echo "$coin"
+coincli="$coin""-cli"
+coind="$coin""d"
+coindir=".""$coin"
+coinconf="$coin"".conf"
+
 rsflag=$( < /home/linaro/restartflag)
 #echo "Flag= $rsflag"
 if (( rsflag == 1 )); then
- echo "restarting bitcoind, please wait"
+ echo "restarting $coind, please wait"
 echo 0 > /home/linaro/restartflag
-/home/linaro/bitcoin-cli stop
+./$coincli stop
 echo "Do not shut down the device until notified"
-$x=$(pgrep -f bitcoind)
+$x=$(pgrep -f $coind)
 while [ "$x" !=  "" ]
 do
   echo -n "."
   sleep 1s
-  x=$(pgrep -f bitcoind)
+  x=$(pgrep -f $coind)
 done
-echo "bitcoin has stopped. restart in 5 seconds"
+echo "$coin has stopped. restart in 5 seconds"
 sleep 10s
-echo "starting bitcoind, pleese wait 15 min"
-/home/linaro/bitcoind -daemon
+echo "starting $coind, pleese wait 15 min"
+./$coind -daemon
 fi
