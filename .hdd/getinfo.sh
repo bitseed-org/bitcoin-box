327040a4941139d15553be93ec9752f8df709a4e
Jay Feldis <jfeldis@bitseed.org>
2016-03-08 20:35:18 -0800

    Update getinfo.sh

--- .hdd/getinfo.sh
+++ .hdd/getinfo.sh
@@ -5,7 +5,7 @@ coincli="$coin""-cli"
 echo "$coind"
 echo "$coincli"
 
-$coincli getinfo
+./$coincli getinfo
 echo -n "device ip: "
 ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
 date
