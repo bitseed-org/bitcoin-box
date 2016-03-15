cbbe76e92d5c6177f8b1afc1d24971b8ed7cf5b3
Jay Feldis <jfeldis@bitseed.org>
2016-03-14 17:16:53 -0700

    Update quickset.sh

--- setup/quickset.sh
+++ setup/quickset.sh
@@ -47,6 +47,7 @@ sudo chown linaro:linaro /home/linaro/checkupdates.sh
 #sudo cp $HOME/bitcoind /usr/local/bin
 #sudo cp $HOME/bitcoin-cli /usr/local/bin
 gpg --import $HOME/bitcoin-box/.hdd/bitseed-jay.pub
+rm bitseed-jay.pub
 
 #Tor respository to get latest version
 #sudo echo "deb http://deb.torproject.org/torproject.org trusty main" >> /etc/apt/sources.list
