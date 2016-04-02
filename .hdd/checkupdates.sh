f6ecb39b8b1281a6efaace78891332b337ae9758
Jay Feldis <jfeldis@bitseed.org>
2016-04-02 00:13:20 -0700

    make auto update by default

--- .hdd/checkupdates.sh
+++ .hdd/checkupdates.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #downloads an update notification script from bitseed.org
 #this script is run by crontab
-rm /home/linaro/v1update.sh
 rm /home/linaro/v2update.sh
-wget https://www.bitseed.org/device/updater/v2update.sh
-chmod 755 /home/linaro/v2update.sh
-echo "update check $(date)" >> cron.log
+auto=$(awk -F ' *= *' '$1=="autoupdate"{print $2}' .bitseed/bitseed.conf)
+if (( $auto == 1 )); then
+   wget https://www.bitseed.org/device/updater/v2update.sh
+   chmod 755 /home/linaro/v2update.sh
+   echo "update check $(date)" >> $HOME/cron.log
+fi   
 exit
