8ca70465aa99f5636404d4dcb74fa80e131b3eaa
Jay Feldis <jfeldis@bitseed.org>
2016-04-02 00:32:40 -0700

    Update checkupdates.sh

--- .hdd/checkupdates.sh
+++ .hdd/checkupdates.sh
@@ -2,7 +2,7 @@
 #downloads an update notification script from bitseed.org
 #this script is run by crontab
 rm /home/linaro/v2update.sh
-auto=$(awk -F ' *= *' '$1=="autoupdate"{print $2}' .bitseed/bitseed.conf)
+auto=$(awk -F ' *= *' '$1=="autoupdate"{print $2}' $HOME/.bitseed/bitseed.conf)
 if (( $auto == 1 )); then
    wget https://www.bitseed.org/device/updater/v2update.sh
    chmod 755 /home/linaro/v2update.sh
