6411019c9666849818bdfe6771d6ffb39628cb03
Jay Feldis <jfeldis@bitseed.org>
2016-05-23 23:13:17 -0700

    Update admin2.1-upgrade.sh

--- setup/admin2.1-upgrade.sh
+++ setup/admin2.1-upgrade.sh
@@ -8,7 +8,17 @@ bash gitclone.sh
 sudo bash $HOME/bitcoin-box/setup/admin-v2.1-install.sh
 
 #install Tor to support new bitcoin onion routing
-
+sudo apt-get install -y tor
+sudo apt-get -y autoremove
+sudo echo "HiddenServiceDir /var/lib/tor/bitseed-service/" >> /etc/tor/torrc
+sudo echo "HiddenServicePort 80 127.0.0.1:82" >> /etc/tor/torrc
+sudo echo "ControlPort 9051" >> /etc/tor/torrc
+sudo echo "CookieAuthentication 1" >> /etc/tor/torrc
+sudo usermod -a -G debian-tor linaro 
+sudo service tor restart
+sleep 5
+echo "Your Onion status page address:"
+sudo cat /var/lib/tor/bitseed-service/hostname
 
 ./btcstop.sh
 
