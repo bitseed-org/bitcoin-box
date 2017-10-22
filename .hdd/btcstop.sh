a87e33718f46f2b7928727abf34192ccc07e0754
Jay Feldis <jfeldis@bitseed.org>
2017-10-22 10:37:56 -0700

    $HOME

--- .hdd/btcstop.sh
+++ .hdd/btcstop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/home/linaro/bitcoin-cli -datadir=/home/linaro/.bitcoin stop
+$HOME/bitcoin-cli -datadir=/home/linaro/.bitcoin stop
 echo "Do not shut down the device until notified"
 x=$(pgrep -f bitcoind)
 while [ "$x" !=  "" ]
