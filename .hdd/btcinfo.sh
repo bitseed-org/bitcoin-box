96633007ac9e4b7f1ddbb74427be50ecdb0fde5f
Jay Feldis <jfeldis@bitseed.org>
2017-10-22 10:37:09 -0700

    Update btcinfo.sh
    $HOME

--- .hdd/btcinfo.sh
+++ .hdd/btcinfo.sh
@@ -1,4 +1,4 @@
-./bitcoin-cli -datadir=/home/linaro/.bitcoin getinfo
+./bitcoin-cli -datadir=$HOME/.bitcoin getinfo
 echo -n "device ip: "
 ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'
 date
