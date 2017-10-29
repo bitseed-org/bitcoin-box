cd1d533551a7e3e0c7e754ccbeb312c49b21963e
Jay Feldis <jfeldis@bitseed.org>
2017-10-29 14:38:39 -0700

    Update safestop.sh

--- .hdd/safestop.sh
+++ .hdd/safestop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #stops bitcoind and shuts down device when /home/linaro/retartflag is set to 2
 #intended to be run by sudo crontab -e  (root crontab) every minute
-rsflag=$( < $HOME/restartflag)
+rsflag=$( < /home/linaro/restartflag)
 #echo "Flag= $rsflag"
 if (( rsflag == 2 )); then
  echo "stopiing bitcoind, please wait"
-echo 0 > $HOME/restartflag
+echo 0 > /home/linaro/restartflag
 $HOME/bitcoin-cli stop
 echo "Do not shut down the device until notified"
 $x=$(pgrep -f bitcoind)
